b9d245d7579b7e96ebf052edc66d5c73169c0ec5
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-15 15:59:22 +0300

    palindrome addition solving

--- palindrome.js
+++ palindrome.js
@@ -6,9 +6,15 @@ const detectPalindrome = str => {
   }
   if (typeof str === 'string' || str instanceof String) {
     const ar = str.toLocaleLowerCase().split('');
-    const middle = (ar.length - 1) / 2;
+    let middle;
+    str.length % 2 === 0
+      ? (middle = ar.length / 2)
+      : (middle = (ar.length - 1) / 2);
     const first = ar.slice(0, middle);
-    const second = ar.slice(middle + 1, ar.length).reverse();
+    let second;
+    middle % 2 === 0
+      ? (second = ar.slice(middle, ar.length).reverse())
+      : (second = ar.slice(middle + 1, ar.length).reverse());
     if (first.join() == second.join()) {
       message = 'This string is palindrome!';
       return message;
@@ -17,9 +23,19 @@ const detectPalindrome = str => {
   }
   return message;
 };
-console.log(detectPalindrome('testtest'));
+console.log(detectPalindrome('Огоміннімого'));
 // detectPalindrom(true) - // "Passed argument is not a string"
 //   detectPalindrom('') - // "String is empty"
 //   detectPalindrom('TARARAT') - // 'This string is palindrome!'
 //   detectPalindrom('I can fly ylf nac i') - // 'This string is palindrome!'
 //   detectPalindrom('testtest'); // 'This string is not a palindrome!'
+
+// if (typeof str === 'string' && str !== '') {
+//   return str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
+//     ? 'This string is palindrome!'
+//     : 'This string is not a palindrome!';
+// } else if (str === '') {
+//   return 'String is empty';
+// } else {
+//   return 'Passed argument is not a string';
+// }
