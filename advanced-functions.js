804db669a6c1342ffddc18a03121cf6bfd64d952
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-20 21:24:52 +0300

    cashe

--- advanced-functions.js
+++ advanced-functions.js
@@ -2,14 +2,29 @@
 // ------------------------------------ TASK №1
 //=============================================
 const cache = func => {
-  // Do your magic here
+  let cache = new Map();
+
+  return function (x) {
+    if (cache.has(x)) {
+      // если кеш содержит такой x,
+      return cache.get(x); // читаем из него результат
+    }
+
+    let result = func(x); // иначе, вызываем функцию
+
+    cache.set(x, result); // и кешируем (запоминаем) результат
+    return result;
+  };
 };
 
 //=============================================
 // ------------------------------------ TASK №2
 //=============================================
 const forwardBackwardSteps = () => {
-  // Do your magic here
+  let step = 0;
+  function forward() {}
+  function backward() {}
+  function revealStep() {}
 };
 
 //=============================================
